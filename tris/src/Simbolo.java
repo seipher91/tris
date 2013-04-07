967d4e552b051975b6240ea398ed5e4fd6b07f02
roberto <roberto@192.168.0.2>
2013-04-07 17:25:04 +0200

    implementazione del simbolo vincolato dal giocatore per poter definire una procedure is() che riconosca il simbolo in casella

--- tris/src/Simbolo.java
+++ tris/src/Simbolo.java
@@ -4,7 +4,21 @@ public class Simbolo {
     private String O;
     
     public Simbolo(){
+        this.X = "X";
+        this.O = "O";
+    }
+    
+    
+    
+    public String setX(){
+        return this.X;
+    }
+    
+    public String setO(){
+        return this.O;
+    }
+    
+    public void is(){
         
     }
-
 }
--- tris/src/player.java
+++ tris/src/player.java
@@ -1,3 +1,6 @@
 public class player {
     
+    private Simbolo simbol;
+    
+    
 }
